60a19d857aad968fd5e2960c2974699a4eecb53a
yxgalaxy <635347773@qq.com>
2017-11-06 19:38:31 +0800

    3.4
    0.写入数据库的效率慢于写入excel?
    1.写入数据库,email索引unique去重

--- getEmailAddressToDb.py
+++ getEmailAddressToDb.py
@@ -1,23 +1,21 @@
 import os
 import xlrd
 import re
-import xlwt
 import datetime
+import pymysql
 
 dataPrevious = {'dataP':[],'dataN':[]}
-parity = 0
 #遍历源数据路径
 def getAllExcelNames(regex,sourcePath,savePath):
     # print(sourcePath,regex)
-    global parity
     for parent, dirnames, filenames in os.walk(sourcePath):
-        print('源文件总数',filenames.__len__())
+        # print('源文件总数',filenames.__len__())
         for filename in filenames:
             if re.match(regex, filename, re.IGNORECASE):
                 excelPathName = os.path.join(parent,filename)#当前文件路径及文件名
                 try:
                     dataPrevious['dataN'] = getDataFromExcel(excelPathName)
-                # print('dataN',dataPrevious['dataN'])
+                    # print('dataN',dataPrevious['dataN'])
                 except Exception as err:
                     print('文件读取出错',err)
                 #     记录错误信息
@@ -26,29 +24,16 @@ def getAllExcelNames(regex,sourcePath,savePath):
                     f.write("\n")
                     f.close()
 
-            # 比较数据是否重复
-            # print('dataP',dataPrevious['dataP'])
-            flag = [i for i in dataPrevious['dataN'] if i in dataPrevious['dataP'] ]    #交集
-            #或者 flag = set(dataPrevious['dataN']) & set(dataPrevious['dataP'])
-            if flag.__len__() > 0:
-                # print('重复-----------------------')
-                # 记录重复文件名:
-                # fw = open(savePath + "repeatLog.txt","a")
-                # fw.write(excelPathName)
-                # fw.write("\n")
-                # fw.close()
-                #利用set,减去交集
-                dataPrevious['dataN'] = dataPrevious['dataN'] - set(flag) # 减去交集
-            #写入表格操作
-            writeExcel(dataPrevious['dataN'],excelPathName,savePath)
+            #写入数据库
+                opearDb(dataPrevious['dataN'],excelPathName)
             dataPrevious['ExcelFile'] = excelPathName  # 存储当前excel名称
             dataPrevious['dataP'] = dataPrevious['dataN']#后读取数据前存
-            parity += 1
+
 #获取数据,返回示例:oneExcelData[[sheet1],[sheet2],,,],返回set
 def getDataFromExcel(excelPathName):
     # 读取文件
     dataCurrentExcel = []
-    print('当前文件路径名:', excelPathName)
+    # print('当前文件路径名:', excelPathName)
     excelCurrent = xlrd.open_workbook(excelPathName)
     # 获取当前excel表所有sheet
     all_sheets_list = excelCurrent.sheet_names()
@@ -122,26 +107,20 @@ def getAllEmail(arg):
             addressList.append(result[i][0])
     return (addressList)
 
-#写入excel
-def writeExcel(data,currentExcelName,savePath):#data可以不需要而直接写入excel提升效率?
-    # print("写ru数据表被执行")
-    global writeCount
-    excelNameCount =  fileCount(savePath) - 1#获取存储文件夹文件数
-    # print('获取到文件数',excelNameCount)
-        # 30000行数据换一张表
-    x = 0#数据列计数
-    for i in range(data.__len__()):
-        if i % 30000 == 0:
-            writeCount += 1
-            x = 0
-            new_workbook = xlwt.Workbook()
-            new_sheet = new_workbook.add_sheet("sheet1")
-            sheet2 = new_workbook.add_sheet("sheet2")
-            sheet2.write(0,0,currentExcelName)
-            excelNameCount += 1
-        new_sheet.write(x, 0, list(data)[i])
-        new_workbook.save(savePath + str(excelNameCount) + ".xls")
-        x += 1
+#连接数据库
+def opearDb(data,filename):
+    global writeCount,db,cursor
+    writeCount += 1
+
+    for i in data:
+        #利用email索引唯一去重
+        try:
+            sql = "INSERT INTO EMAIL (EMAIL,FILENAME) VALUES ('{}','{}')".format(i,filename)
+            cursor.execute(sql)
+            db.commit()
+        except:
+            # print('数据写入错误')
+            continue
 
 # 获取写入目标文件夹的文件数,用于计数命名
 def fileCount(savePath):
@@ -152,10 +131,14 @@ def fileCount(savePath):
 
 if __name__ == "__main__":
     starttime = datetime.datetime.now()
+    #打开数据库
+    db = pymysql.connect("localhost", "root", "", "db3")
+    cursor = db.cursor()
     writeCount = 0
-    # long running
     #遍历全部文件
-    getAllExcelNames('(.)+\.xls|\.xlsx',"E:/testExcelData",r"E:/excelAddressAll/")#正则表达式,数据源路径,目标路径
+    getAllExcelNames('(.)+\.xls|\.xlsx',"E:/testExcelData",r"E:/excelAddressAll/")#正则表达式,数据源路径,错误文件存储路径
+    #关闭数据库
+    db.close()
+    print('写入表数', writeCount)
     endtime = datetime.datetime.now()
     print('邮件列表获取完成,运行时间:',endtime - starttime)
-    print('写入表数', writeCount)
---
+++ pyMysql.py
@@ -0,0 +1,41 @@
+import pymysql
+
+# 打开数据库连接
+db = pymysql.connect("localhost", "root", "", "db3")
+
+# 使用 cursor() 方法创建一个游标对象 cursor
+cursor = db.cursor()
+
+# # 使用 execute() 方法执行 SQL，如果表存在则删除
+# cursor.execute("DROP TABLE IF EXISTS EMPLOYEE")
+#
+# # 使用预处理语句创建表
+# sql = """CREATE TABLE EMPLOYEE (
+#          FIRST_NAME  CHAR(20) NOT NULL,
+#          LAST_NAME  CHAR(20),
+#          AGE INT,
+#          SEX CHAR(1),
+#          INCOME FLOAT )
+#          """
+data = ['nariai_tetsuro@kobelconet.com', 'rchen@visa.com', 'dindo.chu@roche.com','0000',222]
+for i in data:
+    print(i)
+    # sql = "INSERT INTO email (email, filename) VALUES ('"+i+"', '4')"
+    try:
+        sql = "insert into email (email,filename) values ('{0}','{1}')".format(str(i),'99')
+        # 执行sql语句
+        cursor.execute(sql)
+        # 提交到数据库执行
+        # 如果发生错误则回滚
+        db.commit()
+    except:
+        continue
+cursor.execute("SELECT VERSION()")
+
+# 使用 fetchone() 方法获取单条数据.
+data = cursor.fetchone()
+
+print("Database version : %s " % data)
+
+# 关闭数据库连接
+db.close()
